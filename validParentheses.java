599cb81508d18b1340b497974720ce76a5b12aaf
shafiqueismail <shafiqueismail12@gmail.com>
2025-04-04 20:05:05 -0400

    Update validParentheses.java

--- validParentheses.java
+++ validParentheses.java
@@ -1,14 +1,37 @@
 // Question 20: Valid Parentheses
 
 
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
 
-        Stack<String> stack = new Stack<String>();
-        
-        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.peek();
+                if ((ch == ')' && top == '(') ||
+                    (ch == '}' && top == '{') ||
+                    (ch == ']' && top == '[')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
+
 
 /*
 Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
